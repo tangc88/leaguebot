40ca5498cdc0eafd7fb1952af46cb2324ac4a891
Chieh Lun Tang <tangc@colorado.edu>
2017-02-01 09:51:13 -0700

    bug searching

--- bot/messenger.py
+++ bot/messenger.py
@@ -62,7 +62,7 @@ class Messenger(object):
 
     def write_rank(self, channel_id):
         riot = RGAPI-e3c527f3-1921-4ee8-9d6e-087aa21deb76
-        json_games = urllib2.urlopen('https://na.api.pvp.net/api/lol/na/v2.5/league/by-summoner/31203597/entry?api_key=' + riot)
+        #json_games = urllib2.urlopen('https://na.api.pvp.net/api/lol/na/v2.5/league/by-summoner/31203597/entry?api_key=' + riot)
         games = json.load(json_games)
         tier = games['31203597'][0]['tier']
         division = games['31203597'][0]['entries'][0]['division']
