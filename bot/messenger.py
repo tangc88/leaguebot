52586a94100ead77d76586ef43ace9cb24908c1f
Chieh Lun Tang <tangc@colorado.edu>
2017-02-01 16:29:09 -0700

    added second extra

--- bot/messenger.py
+++ bot/messenger.py
@@ -91,8 +91,12 @@ class Messenger(object):
         json_games_jake_champs = urllib2.urlopen('https://na.api.pvp.net/api/lol/na/v1.3/stats/by-summoner/45556126/ranked?season=SEASON2017&api_key=RGAPI-e3c527f3-1921-4ee8-9d6e-087aa21deb76')
         games_jake_champs = json.load(json_games_jake_champs)
         riven_losses = games_jake_champs['champions'][12]['stats']['totalSessionsLost']
+        riven_first_blood = games_jake_champs['champions'][12]['stats']['totalFirstBlood']
         extra_joke = "He has also lost " + str(riven_losses) + " games as Riven. :-1:"
         self.send_message(channel_id, extra_joke)
+        self.clients.send_user_typing_pause(channel_id)
+        extra_joke_two = "Also has " + str(riven_first_blood) + " as Riven. :chart_with_downwards_trend:"
+        self.send_message(channel_id, extra_joke_two)
 
 
     def write_error(self, channel_id, err_msg):
