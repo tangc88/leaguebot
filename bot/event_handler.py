20ffc37a7b87d5d6de0453100e20c1474d7bbf55
Chieh Lun Tang <tangc@colorado.edu>
2017-02-20 14:40:49 -0700

    try to parse with split

--- bot/event_handler.py
+++ bot/event_handler.py
@@ -47,6 +47,7 @@ class RtmEventHandler(object):
                 elif 'joke' in msg_txt:
                     self.msg_writer.write_joke(event['channel'])
                 elif 'weather' in msg_txt:
+                    nothing, zip_code = msg_txt.split(" ", 1)
                     self.msg_writer.write_weather(event['channel'], zip_code)
                 elif 'alternative fact' in msg_txt:
                     self.msg_writer.write_alternative_fact(event['channel'])
