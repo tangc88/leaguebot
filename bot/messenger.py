71ad836c871e2b561f1adf05446ae174c2106aa2
Chieh Lun Tang <tangc@colorado.edu>
2017-03-15 18:04:51 -0600

    removed mistakes in greeting

--- bot/messenger.py
+++ bot/messenger.py
@@ -74,7 +74,7 @@ class Messenger(object):
 
     def write_greeting(self, channel_id, user_id):
         greetings = ['Hi', 'Hello', 'Nice to meet you', 'Howdy', 'Salutations']
-        txt = '<@' + user_id + '>' + '{}, <@{}>!'.format(random.choice(greetings), user_id)
+        txt = '{}, <@{}>!'.format(random.choice(greetings), user_id)
         self.send_message(channel_id, txt)
 
     def write_prompt(self, channel_id):
