0f70f38b1977039cf78aff9aa16e41e8573d77d5
Chieh Lun Tang <tangc@colorado.edu>
2017-03-22 17:05:49 -0600

    added a space by accident

--- bot/event_handler.py
+++ bot/event_handler.py
@@ -48,7 +48,7 @@ class RtmEventHandler(object):
                     self.msg_writer.write_joke(event['channel'])
                 elif 'duo' in msg_txt:
                     botname, duo, person1, person2 = msg_txt.split(" ")
-                    self.msg_writer.write_duo(event['channel'], person1, person 2)
+                    self.msg_writer.write_duo(event['channel'], person1, person2)
                 elif 'weather' in msg_txt:
                     botname, weather, zip_code = msg_txt.split(" ", 2)
                     self.msg_writer.write_weather(event['channel'], zip_code)
