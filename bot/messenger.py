48c339c1e768031f13a01689f9fb42d136c2ef7d
Chieh Lun Tang <tangc@colorado.edu>
2017-03-24 11:22:01 -0600

    less loops

--- bot/messenger.py
+++ bot/messenger.py
@@ -484,7 +484,7 @@ class Messenger(object):
         for element in person1_match_id:
             if element in person2_match_id:
                 common_match_id.append(element)
-        while i < 8:
+        while i < 5:
             json_match = urllib2.urlopen('https://na.api.riotgames.com/api/lol/NA/v2.2/match/' + str(common_match_id[i]) + '?api_key=' + riot)
             match = json.load(json_match)
             while n < 10:
