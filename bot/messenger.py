b33b20e4c930621450d2bb0e72bdb44cb4f81dfc
Trevor Wakeman <trevor.wakeman@colorado.edu>
2017-04-17 16:13:36 -0600

    increased time further

--- bot/messenger.py
+++ bot/messenger.py
@@ -505,7 +505,7 @@ class Messenger(object):
         person1_match_id = []
         person2_match_id = []
         json_match_list_person1 = requests.get('http://mottbot.herokuapp.com/trevor/dave') #+ str(person1) + '/' + str(person2))
-        time.sleep(8)
+        time.sleep(18)
         jsondata = json_match_list_person1.json()
         duo_percentage = jsondata['win_percent']
 
